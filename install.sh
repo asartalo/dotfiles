e86e68cc2518d428cc2d23d6cb8ba288c2abeee3
Wayne Duran <asartalo@gmail.com>
2016-03-24 20:10:19 +0800

    Fixing install script

--- install.sh
+++ install.sh
@@ -5,9 +5,9 @@
 ############################
 
 dir=~/dotfiles                    # dotfiles directory
-files="vim" # list of files/folders to symlink in homedir
+files=".vim" # list of files/folders to symlink in homedir
 
 for file in $files; do
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/$file ~/$file
 done
