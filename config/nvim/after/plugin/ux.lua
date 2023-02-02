68f21eba70d7c46080abf21befc341df6e5fd029
Wayne Duran <asartalo@gmail.com>
2023-02-02 15:15:31 +0800

    nvim: more ux tweaks

--- config/nvim/after/plugin/ux.lua
+++ config/nvim/after/plugin/ux.lua
@@ -1,12 +1,14 @@
+local opt = vim.opt
+
 -- Text wrapping
-vim.opt.wrap = false -- don't wrap text
-vim.opt.textwidth = 0
+opt.wrap = false -- don't wrap text
+opt.textwidth = 0
 
 -- At command line, complete longest common string, then list alternatives. 
-vim.opt.wildmode = "longest,list"
+opt.wildmode = "longest,list"
 
 -- more powerful backspacing
-vim.opt.backspace = "indent,eol,start"
+opt.backspace = "indent,eol,start"
 
 -- Remove trailing spaces on save
 --
@@ -16,3 +18,12 @@ vim.opt.backspace = "indent,eol,start"
 --     autocmd BufWritePre * :%s/\s\+$//e
 --   augroup END
 -- ]]
+
+-- The current buffer can be put to the background without writing to disk
+opt.hidden = true
+
+-- Match and search
+opt.hlsearch   = true -- highlight search
+opt.ignorecase = true -- Do case in sensitive matching with
+opt.smartcase  = true -- be sensitive when there's a capital letter
+opt.incsearch  = true
