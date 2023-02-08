465c7d5a7d2e4bd25c493f9eb1ac6bf6ba4238c8
Wayne Duran <asartalo@gmail.com>
2023-02-08 18:28:18 +0800

    autopairs fix

---
+++ config/nvim/after/plugin/autopairs_setup.lua
@@ -0,0 +1,11 @@
+if NVIM_AUTOPAIRS_INSTALLED then
+  require("nvim-autopairs").setup({})
+
+  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+  local cmp = require('cmp')
+  cmp.event:on(
+    'confirm_done',
+    cmp_autopairs.on_confirm_done()
+  )
+
+end
--- config/nvim/lua/custom/plugins/autopairs.lua
+++ config/nvim/lua/custom/plugins/autopairs.lua
@@ -2,7 +2,11 @@ return function (use)
   use {
     "windwp/nvim-autopairs",
     config = function ()
-      require("nvim-autopairs").setup({})
+      -- For some reason, this is not called.
+      -- See: https://github.com/windwp/nvim-autopairs/issues/284
+      print("nvim-autopairs config setup called")
+      -- require("nvim-autopairs").setup({})
     end
   }
+  NVIM_AUTOPAIRS_INSTALLED = true
 end
