96c9ddc3a7a27878665a8dcb541ed82ea2930b1a
Wayne Duran <asartalo@gmail.com>
2023-02-15 10:32:09 +0800

    nvim: add emmet for htmldjango

--- config/nvim/lua/custom/plugins/emmet.lua
+++ config/nvim/lua/custom/plugins/emmet.lua
@@ -9,7 +9,16 @@ return function (_)
   lspconfig.emmet_ls.setup({
     -- on_attach = on_attach,
     capabilities = capabilities,
-    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
+    filetypes = {
+      'html',
+      'htmldjango',
+      'typescriptreact',
+      'javascriptreact',
+      'css',
+      'sass',
+      'scss',
+      'less'
+    },
     init_options = {
       html = {
         options = {
