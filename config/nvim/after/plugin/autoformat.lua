982d914a544191ef43a65bb39b79403b8a887818
Wayne Duran <asartalo@gmail.com>
2024-03-15 14:22:16 +0800

    removing unnecessary debug print

--- config/nvim/after/plugin/autoformat.lua
+++ config/nvim/after/plugin/autoformat.lua
@@ -1,15 +1,10 @@
 local autoformat_files = {
-  "*.go",
-  "*.rs",
-  "*.js",
-  "*.ts"
+	"*.go",
+	"*.rs",
+	"*.js",
+	"*.ts",
 }
 
 for _, filetype in ipairs(autoformat_files) do
-  vim.cmd(
-    [[ autocmd BufWritePre ]] .. filetype .. [[ lua vim.lsp.buf.formatting_sync() ]]
-  )
+	vim.cmd([[ autocmd BufWritePre ]] .. filetype .. [[ lua vim.lsp.buf.formatting_sync() ]])
 end
-
-
-print("HELLO from autoformat.lua")
